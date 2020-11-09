1a89a2b1c5ebd863e95113d530cd37554c35f2fe
matned <mat.niedbal@yahoo.com>
2020-11-09 23:22:35 +0100

    PositionDTO refactor - static methods and Position details added.

--- src/main/java/eu/mrndesign/www/matned/dto/PositionDTO.java
+++ src/main/java/eu/mrndesign/www/matned/dto/PositionDTO.java
@@ -3,25 +3,51 @@ package eu.mrndesign.www.matned.dto;
 import eu.mrndesign.www.matned.dto.audit.AuditDTO;
 import eu.mrndesign.www.matned.model.audit.AuditInterface;
 import eu.mrndesign.www.matned.model.company.Position;
+import eu.mrndesign.www.matned.model.company.PositionDetails;
 
 import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+import static eu.mrndesign.www.matned.Patterns.DATE_FORMATTER;
 
 public class PositionDTO implements DTOEintityDescriptionImplementation{
 
     public static PositionDTO applyWithAudit(Position entity) {
-        return new PositionDTO(entity.getEntityDescription().getName(), entity.getEntityDescription().getDescription(),
-                AuditInterface.apply(entity));
+        PositionDTO dto = apply(entity);
+        dto.auditDTO = AuditInterface.apply(entity);
+        return dto;
     }
 
     public static PositionDTO apply(Position entity){
-        return new PositionDTO(entity.getEntityDescription().getName(), entity.getEntityDescription().getDescription());
+        return new PositionDTO(
+                entity.getEntityDescription() != null ? entity.getEntityDescription().getName() : null,
+                entity.getEntityDescription() != null ? entity.getEntityDescription().getDescription() : null);
     }
+
+    public static PositionDTO applyWithAudit(Position entity, PositionDetails details) {
+        PositionDTO dto = apply(entity, details);
+        dto.detailsAuditDTO = AuditInterface.apply(details);
+        return dto;
+    }
+
+    public static PositionDTO apply(Position entity,  PositionDetails details){
+        PositionDTO dto = apply(entity);
+        String creationDate = details.getPositionCreationDate() != null ? details.getPositionCreationDate().format(DATE_FORMATTER) : null;
+        String deactivationDate = details.getPositionDeactivationDate() != null ? details.getPositionDeactivationDate().format(DATE_FORMATTER) : null;
+        dto.positionCreationDate = creationDate;
+        dto.positionDeactivationDate = deactivationDate;
+        dto.isActive = details.isActive();
+        return dto;    }
 
     @NotEmpty(message = "This field cannot be empty")
     private String name;
     @NotEmpty(message = "This field cannot be empty")
     private String description;
+    private String positionCreationDate;
+    private String positionDeactivationDate;
+    boolean isActive;
     private AuditDTO auditDTO;
+    private AuditDTO detailsAuditDTO;
 
     public PositionDTO() {
     }
@@ -31,26 +57,32 @@ public class PositionDTO implements DTOEintityDescriptionImplementation{
         this.description = description;
     }
 
-    public PositionDTO(String name, String description, AuditDTO auditDTO) {
-        this.name = name;
-        this.description = description;
-        this.auditDTO = auditDTO;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public String getPositionCreationDate() {
+        return positionCreationDate;
+    }
+
+    public String getPositionDeactivationDate() {
+        return positionDeactivationDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public AuditDTO getAuditDTO() {
+        return auditDTO;
+    }
+
+    public AuditDTO getDetailsAuditDTO() {
+        return detailsAuditDTO;
     }
 
     @Override
@@ -58,7 +90,11 @@ public class PositionDTO implements DTOEintityDescriptionImplementation{
         return "PositionDTO{" +
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", positionCreationDate='" + positionCreationDate + '\'' +
+                ", positionDeactivationDate='" + positionDeactivationDate + '\'' +
+                ", isActive=" + isActive +
                 ", auditDTO=" + auditDTO +
+                ", detailsAuditDTO=" + detailsAuditDTO +
                 '}';
     }
 }
--- src/main/java/eu/mrndesign/www/matned/model/company/PositionDetails.java
+++ src/main/java/eu/mrndesign/www/matned/model/company/PositionDetails.java
@@ -1,5 +1,6 @@
 package eu.mrndesign.www.matned.model.company;
 
+import eu.mrndesign.www.matned.model.audit.AuditInterface;
 import eu.mrndesign.www.matned.model.audit.BaseEntity;
 import eu.mrndesign.www.matned.model.common.EntityDescription;
 import eu.mrndesign.www.matned.model.common.EntityDescriptionImplementation;
@@ -11,16 +12,61 @@ import javax.persistence.EntityListeners;
 import javax.persistence.ManyToMany;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
-public class PositionDetails  extends BaseEntity{
+public class PositionDetails  extends BaseEntity implements AuditInterface {
 
     LocalDate positionCreationDate;
     LocalDate positionDeactivationDate;
+    boolean isActive;
     @ManyToMany
     List<Inventory> inventories;
-    boolean isActive;
 
+    public PositionDetails() {
+    }
 
+    public LocalDate getPositionCreationDate() {
+        return positionCreationDate;
+    }
+
+    public LocalDate getPositionDeactivationDate() {
+        return positionDeactivationDate;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PositionDetails that = (PositionDetails) o;
+        return isActive == that.isActive &&
+                Objects.equals(positionCreationDate, that.positionCreationDate) &&
+                Objects.equals(positionDeactivationDate, that.positionDeactivationDate) &&
+                Objects.equals(inventories, that.inventories);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), positionCreationDate, positionDeactivationDate, isActive, inventories);
+    }
+
+    @Override
+    public String toString() {
+        return "PositionDetails{" +
+                "positionCreationDate=" + positionCreationDate +
+                ", positionDeactivationDate=" + positionDeactivationDate +
+                ", isActive=" + isActive +
+                ", inventories=" + inventories +
+                '}';
+    }
 }
